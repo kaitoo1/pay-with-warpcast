abe818d213644343d949c69ff188025cdf5a8a80
kaito <80802871+kaitoo1@users.noreply.github.com>
2025-03-23 01:18:07 +0900

    clean up frame metadata

--- src/app/frame/page.tsx
+++ src/app/frame/page.tsx
@@ -4,40 +4,31 @@ import { Metadata } from "next";
 import FrameAppContainer from "~/components/frame/FrameAppContainer";
 import { APP_URL } from "../page";
 
+const frame = {
+  version: "next",
+  imageUrl: `${APP_URL}/opengraph-image`,
+  button: {
+    title: "Launch Frame",
+    action: {
+      type: "launch_frame",
+      name: "Pay With Warpcast",
+      url: `${APP_URL}/frame`,
+      splashImageUrl: `${APP_URL}/splash.png`,
+      splashBackgroundColor: "#000000",
+    },
+  },
+};
 export async function generateMetadata(): Promise<Metadata> {
-  try {
-    const frame = {
-      version: "next",
-      imageUrl: `${APP_URL}/opengraph-image`,
-      button: {
-        title: "Launch Frame",
-        action: {
-          type: "launch_frame",
-          name: "Pay With Warpcast",
-          url: `${APP_URL}/frame`,
-          splashImageUrl: `${APP_URL}/splash.png`,
-          splashBackgroundColor: "#000000",
-        },
-      },
-    };
-
-    return {
+  return {
+    title: "Pay With Warpcast",
+    openGraph: {
       title: "Pay With Warpcast",
-      openGraph: {
-        title: "Pay With Warpcast",
-        description: "by Kaito",
-      },
-      other: {
-        "fc:frame": JSON.stringify(frame),
-      },
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      title: "Error",
-      description: "Error loading moshi",
-    };
-  }
+      description: "by Kaito",
+    },
+    other: {
+      "fc:frame": JSON.stringify(frame),
+    },
+  };
 }
 
 // All Frame screens are rendered here with the new navigation system
